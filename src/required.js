34d7bdc02161472ad55e5dc120b9bae4d4a6df0e
Aaron Ladage <aladage@degdigital.com>
2016-12-13 22:13:15 -0600

    first rule commit

---
+++ src/required.js
@@ -0,0 +1,57 @@
+let required = function() {
+    
+    let messages = {
+            requiredMsg: {
+                attribute: 'data-validation-required-message',
+                message: 'This field is required.'
+            }
+        },
+        events = [
+            'focusout',
+            'submit'
+        ];
+
+    function getEvents() {
+        return events;
+    };
+
+    function isRelevant(containerEl, inputEls) {
+        return inputEls.every(function(el) {
+            return el.getAttribute('required') !== null;
+        });
+    };
+
+    function validate(matchingField) {
+        return new Promise(function(resolve, reject) {
+            let inputEls = matchingField.inputEls;
+            if (inputEls) {
+                let isValid = inputEls.every(function(el) {
+                    return ((el.value) && (el.value.length > 0));
+                };
+                if (isValid) {
+                    resolve({
+                        valid: true
+                    });
+                } else {
+                    resolve({
+                        valid: false,
+                        message: messages.requiredMsg,
+                        matchingField: matchingField
+                    });
+                }
+            } else {
+                reject('no inputs');
+            }
+            
+        });
+    };
+
+    return {
+        events: getEvents,
+        isRelevant: isRelevant,
+        validate: validate
+    };
+
+};
+
+export default required;
